1dca7107652805ead8444174bb28aa1256ca9ec4
mateuszhermann <majonez15066@wp.pl>
2022-11-13 22:58:13 +0100

    nowy plik:       src/dodawanie.html 	zmieniono:       src/index.css 	zmieniono:       src/index.html 	zmieniono:       src/mnozenie.html 	nowy plik:       src/odejmowanie.html 	nowy plik:       src/resources/Panda_Big.svg 	nowy plik:       src/resources/Sheep_Big.png 	nowy plik:       src/resources/Sheep_Big.svg 	zmieniono:       src/scoreControler.js 	nowy plik:       src/scripts/subControllerDd.js 	nowy plik:       src/scripts/subControllerMn.js 	nowy plik:       src/scripts/subControllerOd.js

---
+++ src/scripts/subControllerOd.js
@@ -0,0 +1,52 @@
+import {randomize} from "./functions.js";
+import {close} from "./controls.js"
+var number=1;
+var a=0;
+var b=0;
+var eql;
+var good=0;
+var bad=0;
+var awnser=0;
+function set(){
+    
+    document.getElementById('number').innerHTML=number;
+    a=randomize();
+    document.getElementById('a').innerHTML=a;
+    b=randomize();
+    document.getElementById('b').innerHTML=b;
+    eql=a-b;
+}
+
+function next(){
+    awnser=document.getElementById("awnser").value;
+    if(eql==awnser){
+        good++
+    } else{
+        bad++
+    }
+    console.log("good"+good+"bad"+bad);
+    number++
+    if(number<=5){
+        set();
+    }else{
+        var score={'good':good, 'bad':bad};
+        localStorage.setItem('score',JSON.stringify(score));
+        window.location.replace('./koniecGry.html');
+    }
+}
+
+
+document.querySelector("#nextButton").addEventListener("click", ()=>{
+    next();
+})
+
+document.querySelector("#close").addEventListener("click", ()=>{
+    close();
+})
+document.querySelector("#menu").addEventListener("click", ()=>{
+    window.location.replace('./index.html')
+})
+
+window.onload = function() {
+    set()
+}
