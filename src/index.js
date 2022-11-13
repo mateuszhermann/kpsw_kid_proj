ac41ab0b9a61895e3855f3f3bb2b284738f70122
mateuszhermann <majonez15066@wp.pl>
2022-11-13 11:42:37 +0100

    zmieniono:       src/index.css 	zmieniono:       src/index.html 	zmieniono:       src/index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1440,
+    height: 1024,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -19,8 +19,8 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.removeMenu()
+  
 };
 
 // This method will be called when Electron has finished
