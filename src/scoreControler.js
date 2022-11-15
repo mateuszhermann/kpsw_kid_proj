8c724eb2d8216a10895d6012825214faaeadaa5e
Krollka <kroll-marika@outlook.com>
2022-11-15 20:26:52 +0100

    stars

--- src/scoreControler.js
+++
@@ -1,8 +0,0 @@
-
-window.onload=function(){
-    var score = JSON.parse(localStorage.getItem("score"));
-    var good = score["good"];
-    var bad = score['bad'];
-    localStorage.removeItem('score'); 
-    console.log(bad);
-}
--- src/scripts/scoreControler.js
+++ src/scripts/scoreControler.js
@@ -1,11 +1,22 @@
 
-// window.onload=function(){
-//     var score = JSON.parse(localStorage.getItem("score"));
-//     var good = score["good"];
-//     var bad = score['bad'];
-//     localStorage.removeItem('score'); 
-//     console.log(bad);
-// }
+window.onload=function(){
+    var score = JSON.parse(localStorage.getItem("score"));
+    var good = score["good"];
+    var bad = score['bad'];
+
+    console.log(good+bad);
+    for (var i = 1; i <=5; i++) {
+        if (good>=i) {
+            document.getElementById("gs"+i).style.color="pink";
+            console.log("gs"+i);
+        }
+        if (bad>=i) {
+            document.getElementById("bs"+i).style.color="pink";
+            console.log("bs"+i);
+        }
+    }
+    localStorage.removeItem('score'); 
+}
 
 document.querySelector("#menu").addEventListener("click", ()=>{
     window.location.replace('./index.html')
